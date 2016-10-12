e9085fb5e676ea7d47c01b615427f302ca151438
okputadora <okputadora@gmail.com>
2016-10-12 17:11:25 -0400

    Add documentation to js file

--- homepage.js
+++ homepage.js
@@ -7,13 +7,20 @@ carousel();
 
 function carousel(){
   arrayIndex += 1;
+  //If we're outside of the array go back to the beginning 
   if (arrayIndex > 3) {
     arrayIndex = 0;
-    console.log("if statement active");
   };
+  //change current slide to the next image in the array
   document.getElementById("currentSlide").src = imgArray[arrayIndex];
+  //
+  var tickerArray = document.getElementsByClassName("circle-1");
+
+  //Debugging can be removed later
   console.log(imgArray[arrayIndex]);
   console.log(arrayIndex);
+  //Putting the timeout function inside the function that is passed
+  //to it (i.e. carousel) creates a loop...as it continually calls itself
   setTimeout(carousel, 4000);
 }
 
