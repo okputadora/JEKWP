502bca80c49155973c3b3c251874b2ab0591e879
okputadora <okputadora@gmail.com>
2016-10-12 17:28:25 -0400

    Match ticker to images (removed unused code in html/css)

--- homepage.js
+++ homepage.js
@@ -1,24 +1,26 @@
 
 var imgArray = ["lunarspacezoom.jpg", "oldbirdzoom.jpg",
                 "resinmanzoom.jpg", "healthcarezoom.jpg"];
-
+var tickerArray = document.getElementsByClassName("circle-1");
 var arrayIndex = -1;
 carousel();
 
 function carousel(){
   arrayIndex += 1;
-  //If we're outside of the array go back to the beginning 
+  //If we're outside of the array go back to the beginning
   if (arrayIndex > 3) {
     arrayIndex = 0;
   };
   //change current slide to the next image in the array
   document.getElementById("currentSlide").src = imgArray[arrayIndex];
-  //
-  var tickerArray = document.getElementsByClassName("circle-1");
-
+  //Reset all of the other tickers back to orihinal color
+  for (var i = 0; i < 4; i++){
+    tickerArray[i].style.backgroundColor = "white";
+  };
+  //change the color of the ticker to match image
+  tickerArray[arrayIndex].style.backgroundColor = "grey";
   //Debugging can be removed later
   console.log(imgArray[arrayIndex]);
-  console.log(arrayIndex);
   //Putting the timeout function inside the function that is passed
   //to it (i.e. carousel) creates a loop...as it continually calls itself
   setTimeout(carousel, 4000);
