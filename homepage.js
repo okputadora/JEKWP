289a3d364da513f959e7184d74d5f21641d711e0
okputadora <okputadora@gmail.com>
2016-10-13 11:26:14 -0400

    Complete overhaul of slideshow
    Rather than cycling through an array of images in JS the images
    are all placed in the HTML. JS changes their class names so that
    one is in the class 'showing' at all times where opacity: 1 and the rest
    have an opacity of 0. This makes it easy to fade between them because
    we can just use CSS transition on the property opacity, rather than
    doing it with JS or JQuery.
    Based off of:
    https://www.sitepoint.com
    /make-a-simple-javascript-slideshow-without-jquery/

--- homepage.js
+++ homepage.js
@@ -1,30 +1,9 @@
+var slides = document.querySelectorAll('#slideContainer .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 4000);
 
-var imgArray = ["lunarspacezoom.jpg", "oldbirdzoom.jpg",
-                "resinmanzoom.jpg", "healthcarezoom.jpg"];
-var tickerArray = document.getElementsByClassName("circle-1");
-var arrayIndex = -1;
-carousel();
-
-function carousel(){
-  arrayIndex += 1;
-  //If we're outside of the array go back to the beginning
-  if (arrayIndex > 3) {
-    arrayIndex = 0;
-  };
-  //change current slide to the next image in the array
-  document.getElementById("currentSlide").src = imgArray[arrayIndex];
-  //Reset all of the other tickers back to orihinal color
-  for (var i = 0; i < 4; i++){
-    tickerArray[i].style.backgroundColor = "white";
-  };
-  //change the color of the ticker to match image
-  tickerArray[arrayIndex].style.backgroundColor = "grey";
-  //Debugging can be removed later
-  console.log(imgArray[arrayIndex]);
-  //Putting the timeout function inside the function that is passed
-  //to it (i.e. carousel) creates a loop...as it continually calls itself
-  setTimeout(carousel, 4000);
+function nextSlide() {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide + 1)%slides.length;
+  slides[currentSlide].className = 'slide showing';
 }
-
-console.log("Hello");
-console.log(imgArray);
