706d6d868f79cdc194777f1bf6a49ac778b75d71
okputadora <okputadora@gmail.com>
2016-10-15 21:59:30 -0400

    Make slideshow larger

--- homepage.js
+++ homepage.js
@@ -1,17 +1,22 @@
+//MENU ON HOVER
+
+
+//SLIDESHOW
 var slides = document.querySelectorAll('#slideContainer .slide');
 var currentSlide = 0;
 var ticker = document.getElementsByClassName("circle-1");
-ticker[0].style.backgroundColor = "black";
+ticker[0].style.backgroundColor = "white";
 var slideInterval = setInterval(nextSlide, 4000);
-
+console.log(slides);
 
 
 function nextSlide() {
-  for (var i = 0; i < 4; i++){
-    ticker[i].style.backgroundColor = "#FFF9E6";
-  }
-  slides[currentSlide].className = 'slide';
-  currentSlide = (currentSlide + 1)%slides.length;
-  slides[currentSlide].className = 'slide showing';
+  //Change the current slide back to hidden and ticker back to white
   ticker[currentSlide].style.backgroundColor = "black";
+  slides[currentSlide].className = 'slide';
+  //Increment slide
+  currentSlide = (currentSlide + 1)%slides.length;
+  //Reveal slide and change ticker to black
+  slides[currentSlide].className = 'slide showing';
+  ticker[currentSlide].style.backgroundColor = "white";
 }
