b9d2a6b5dcf11a0c95fda5b984d6c08562cc79fe
okputadora <okputadora@gmail.com>
2016-10-24 10:09:08 -0400

    Change slideshow to background images (so they resize with window)
    Broke the ticker, fade function, and shadow of header

--- homepage.js
+++ homepage.js
@@ -2,18 +2,19 @@
 
 
 //SLIDESHOW
-var slides = document.querySelectorAll('#slideContainer .slide');
+var slides = ["faceszoom.jpg", "abstractzoom.jpg", "hollowshapeszoom.jpg",
+              "lunarspacezoom.jpg", "degradation1.jpg", "degradation1.jpg",
+              "deagradation2.jpg", "degradation3.jpg"];
 var currentSlide = 0;
 var ticker = document.getElementsByClassName("circle-1");
 ticker[0].style.backgroundColor = "white";
 var slideInterval = setInterval(nextSlide, 4000);
 console.log(slides);
-
-
 function nextSlide() {
   //Change the current slide back to hidden and ticker back to white
   ticker[currentSlide].style.backgroundColor = "black";
-  slides[currentSlide].className = 'slide';
+  document.getElementById("slideshow").style.backgroundImage =
+  "url(" + slides[currentSlide] + ")";
   //Increment slide
   currentSlide = (currentSlide + 1)%slides.length;
   //Reveal slide and change ticker to black
